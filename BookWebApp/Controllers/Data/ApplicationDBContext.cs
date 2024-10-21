1a412571d9d00b4054c38553d0448495a80e4ca4
Fevzi Berk Çeliktaş <fevzi.celiktas@tedu.edu.tr>
2024-10-21 05:27:41 +0300

    Add project files.

---
+++ BookWebApp/Controllers/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using BookWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookWebApp.Controllers.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+                
+        }
+        public DbSet<Category> Categories { get; set; }  
+    }
+}
